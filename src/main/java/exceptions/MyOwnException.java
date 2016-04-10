362a9fb5944ec5a023041caae77b3c609c3d809c
Keercat <knight4ce@aliyun.com>
2016-04-10 09:55:54 +0800

    nothing

--- src/main/java/exceptions/MyOwnException.java
+++ src/main/java/exceptions/MyOwnException.java
@@ -2,6 +2,38 @@ package exceptions;
 
 public class MyOwnException extends Exception {
 
-    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public MyOwnException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public MyOwnException(
+            String message,
+            Throwable cause,
+            boolean enableSuppression,
+            boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+    public MyOwnException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public MyOwnException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public MyOwnException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
 
 }
